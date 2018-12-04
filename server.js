771f8ce73055fc6f437afe702d4ea516b7cf0791
Volkan Arıslı <volkanarisli@gmail.com>
2018-12-04 10:13:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var morgan = require("morgan");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+
+var User = require("./models/user");
+
+var app = express();
+
+
+
+mongoose.connect("mongodb://volkanarisli:volk0198@ds123224.mlab.com:23224/ecommerce", {
+    useNewUrlParser: true
+}, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to the database");
+    }
+});
+
+//Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/create-user", function (req, res) {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    user.save(function (err) {
+        if (err) return next(err);
+        res.json("Succesfully created a new user");
+    });
+});
+
+
+app.listen(3000, function (err) {
+    if (err) throw err;
+    console.log("Server is Running on port 3000");
+});
