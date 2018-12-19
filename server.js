caa0e1ce327772d8d5b8a926e1c392e05416bc93
Volkan Arıslı <volkanarisli@gmail.com>
2018-12-19 10:33:19 +0300

    :bookmark: Login added

--- server.js
+++ server.js
@@ -1,63 +1,60 @@
-var express = require("express");
-var morgan = require("morgan");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var ejs = require("ejs");
-var ejs_mate = require("ejs-mate");
-var session = require("express-session");
-var cookieParser = require("cookie-parser");
-var flash = require("express-flash");
-var MongoStroe = require("connect-mongo");
-var passport = require("passport");
 
-var secret = require("./config/secret")
-var User = require("./models/user");
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+
+
+var secret = require('./config/secret');
+var User = require('./models/user');
 
 var app = express();
 
-
-
-mongoose.connect(secret.database, {
-    useNewUrlParser: true
-}, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected to the database");
-    }
+mongoose.connect(secret.database, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
 });
 
-//Middleware
-app.use(express.static(__dirname + "/public"));
-app.use(morgan("dev"));
+// Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: secret.secretKey,
-    store: new MongoStroe({
-        url: secret.database,
-        autoReconnect: true
-    })
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey,
+  store: new MongoStore({ url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
-app.engine("ejs", ejs_mate);
-app.set("view engine", "ejs");
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
-var mainRoutes = require("./routes/main");
-var userRoutes = require("./routes/user");
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
 app.use(mainRoutes);
-app.use(userRoutes)
+app.use(userRoutes);
 
-
-
-app.listen(secret.port, function (err) {
-    if (err) throw err;
-    console.log("Server is Running on port " + secret.port);
+app.listen(secret.port, function(err) {
+  if (err) throw err;
+  console.log("Server is Running on port " + secret.port);
 });
