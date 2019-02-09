3293201571b8493bc1c4abce3e42b84df844e112
Volkan Arıslı <volkanarisli@gmail.com>
2019-02-09 17:48:23 +0300

    Search feature added

--- models/product.js
+++ models/product.js
@@ -1,15 +1,21 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
+var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
-
 
 var ProductSchema = new Schema({
     category: {
         type: Schema.Types.ObjectId,
-        ref: "Category"
+        ref: 'Category'
     },
-    name: String,
-    Price: Number,
+    name:  String,
+    price: Number,
     image: String
 });
 
-module.exports = mongoose.model("Product", ProductSchema);
+ProductSchema.plugin(mongoosastic, {
+    hosts: [
+        'localhost:9200'
+    ]
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
